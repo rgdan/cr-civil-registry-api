b4f6f269350bcddd43ad82af543898ac71fba9af
Dani <169130542+rgdan@users.noreply.github.com>
2024-05-14 02:33:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Costa Rica National Registry API
+
+API written in Python to get info from [Costa Rica's national registry website](https://servicioselectorales.tse.go.cr/chc/).
+
+## Usage
+
+### ID
+
+Get information from the registry's website from an ID number.
+
+#### Example
+
+`https://example.com/lookup?id=000000000`
+
+```
+{
+  "alias": " ",
+  "birthday": "15/12/1986",
+  "death_date": "",
+  "father": "FREDDY ANTONIO NAVAS VIQUEZ",
+  "father_id_number": "0",
+  "full_name": "KEILOR ANTONIO NAVAS  GAMBOA",
+  "homeless": "NO",
+  "id_number": "113000998",
+  "mother": "SANDRA GAMBOA GUZMAN",
+  "mother_id_number": "106910100",
+  "nationality": "COSTARRICENSE"
+}
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ national_registry_api.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+from playwright.sync_api import Playwright, sync_playwright
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+def parse_html_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    data = {
+        "id_number": soup.find("span", {"id": "lblcedula"}).text if soup.find("span", {"id": "lblcedula"}) else "N/A",
+        "full_name": soup.find("span", {"id": "lblnombrecompleto"}).text if soup.find("span", {"id": "lblnombrecompleto"}) else "N/A",
+        "birthday": soup.find("span", {"id": "lblfechaNacimiento"}).text if soup.find("span", {"id": "lblfechaNacimiento"}) else "N/A",
+        "nationality": soup.find("span", {"id": "lblnacionalidad"}).text if soup.find("span", {"id": "lblnacionalidad"}) else "N/A",
+        "alias": soup.find("span", {"id": "lblconocidocomo"}).text if soup.find("span", {"id": "lblconocidocomo"}) else "N/A",
+        "homeless": soup.find("span", {"id": "lblLeyendaMarginal"}).text if soup.find("span", {"id": "lblLeyendaMarginal"}) else "N/A",
+        "death_date": soup.find("span", {"id": "lbldefuncion2"}).text if soup.find("span", {"id": "lbldefuncion2"}) else "N/A",
+        "father": soup.find("span", {"id": "lblnombrepadre"}).text if soup.find("span", {"id": "lblnombrepadre"}) else "N/A",
+        "father_id_number": soup.find("span", {"id": "lblid_padre"}).text if soup.find("span", {"id": "lblid_padre"}) else "N/A",
+        "mother": soup.find("span", {"id": "lblnombremadre"}).text if soup.find("span", {"id": "lblnombremadre"}) else "N/A",
+        "mother_id_number": soup.find("span", {"id": "lblid_madre"}).text if soup.find("span", {"id": "lblid_madre"}) else "N/A"
+    }
+    return data
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/lookup', methods=['GET'])
+def lookup():
+    id_number = request.args.get('id')
+    if not id_number:
+        return jsonify({"error": "ID number is required"}), 400
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx")
+        page.get_by_placeholder("Digite el número de cédula ahora").click()
+        page.get_by_placeholder("Digite el número de cédula ahora").fill(id_number)
+        page.get_by_role("button", name="Consultar").click()
+        page.wait_for_timeout(1000)
+
+        html_content = page.content()
+        data = parse_html_content(html_content)
+
+        context.close()
+        browser.close()
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
